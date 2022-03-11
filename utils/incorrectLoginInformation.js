0278a55721bb3111d86983ea5d4a2e2ccba1bb65
aclelandx <95543064+aclelandx@users.noreply.github.com>
2022-03-11 13:53:27 -0500

    Added in the View for creating a new post along with a skeleton js file in the public folder

---
+++ utils/incorrectLoginInformation.js
@@ -0,0 +1,5 @@
+function incorrectLoginInformation() {
+    document.getElementById(`incorrectInformation`).innerHTML = `The Email Address, Or the password was incorrect. <br> Please Try Again.`
+}
+
+module.exports = incorrectLoginInformation
